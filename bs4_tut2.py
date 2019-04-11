b3318a68f796daf2b7c282b58866b2821b4dd8ba
Vidumini <heartyvds@gmail.com>
2019-04-11 14:58:56 +0530

    urls in nav bar

---
+++ bs4_tut2.py
@@ -0,0 +1,11 @@
+import bs4 as bs
+import urllib.request
+
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(source, 'lxml')
+
+nav = soup.nav
+for url in nav.find_all('a'):
+    print(url.get('href'))
+
+
