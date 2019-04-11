8caaceddfbb2ea6f2742a84ab76de139720169f6
Vidumini <heartyvds@gmail.com>
2019-04-11 15:48:31 +0530

    extracting table

---
+++ bs4_tut3.py
@@ -0,0 +1,14 @@
+import bs4 as bs
+import urllib.request
+
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(source, 'lxml')
+
+table = soup.find('table')
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
+
