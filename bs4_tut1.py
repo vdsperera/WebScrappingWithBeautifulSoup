1930da035573142bd03bf195bffa6a49eb28e4b9
Vidumini <heartyvds@gmail.com>
2019-04-11 14:39:14 +0530

    finding text and urls

--- bs4_tut1.py
+++ bs4_tut1.py
@@ -3,4 +3,11 @@ import urllib.request
 
 source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 soup = bs.BeautifulSoup(source, 'lxml')
-print(soup.find_all('p'))
+#print(soup.find_all('p'))
+# for paragraph in soup.find_all('p'):
+#     print(paragraph.text)
+#print(soup.get_text())
+for url in soup.find_all('a'):
+    print(url.get('href'))
+
+
