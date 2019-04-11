651db3aaed0b1b6aeafa7d454ba2c20e3c20bb88
Vidumini <heartyvds@gmail.com>
2019-04-11 13:58:33 +0530

    basic scrapping

---
+++ bs4_tut1.py
@@ -0,0 +1,6 @@
+import bs4 as bs
+import urllib.request
+
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(source, 'lxml')
+print(soup.find_all('p'))
