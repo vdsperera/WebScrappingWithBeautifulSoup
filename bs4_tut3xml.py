5395684653071a7eb6807601e6f550efb361fa5e
Vidumini <heartyvds@gmail.com>
2019-05-02 13:08:02 +0530

    extracting table from xml

--- bs4_tut3xml.py
+++ bs4_tut3xml.py
@@ -1,9 +1,18 @@
 import bs4 as bs
 import urllib.request
 
-source = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
-soup = bs.BeautifulSoup(source, 'xml')
-for url in soup.find_all('loc'):
-    print(url.text)
+# source = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
+# soup = bs.BeautifulSoup(source, 'xml')
+# for url in soup.find_all('loc'):
+#     print(url.text)
 
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(source, 'lxml')
+table = soup.table
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
 
